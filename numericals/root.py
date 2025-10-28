c4756679da0b44822b193aaa17033d3fc09faa63
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2025-10-28 00:06:47 -0400

    Changed layout to flat and added tests

--- numericals/root.py
+++ numericals/root.py
@@ -1,17 +1,19 @@
-def bisection(function, alpha, beta, tolerance=10*-10, max_iterations=100_000):
+def bisection(function, alpha, beta, tolerance=10*-10, max_iterations=10_000):
     if function(alpha) == 0:
         return alpha
     if function(beta) == 0:
         return beta
     if function(alpha) * function(beta) > 0:
         raise ValueError
+
     for _ in range(max_iterations):
-        midpoint = (alpha + beta) / 2.0
+        midpoint = (alpha + beta) * 0.5
         fm = function(midpoint)
-        if abs(beta - alpha) / 2.0 < tolerance or fm == 0:
+        if (abs(beta - alpha) * 0.5 < tolerance) or (fm == 0):
             return midpoint
         if fm * function(alpha) < 0:
             beta = midpoint
         else:
             alpha = midpoint
-    return (alpha + beta) / 2.0
+
+    return (alpha + beta) * 0.5
