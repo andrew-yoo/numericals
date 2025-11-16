2000d9095bdc1b9826e2065ea3f6199df3832507
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2025-11-15 20:15:07 -0500

    Reformatted

--- numericals/ode.py
+++ numericals/ode.py
@@ -79,7 +79,9 @@ def heun(
     return values
 
 
-def rk4(function: Callable[[float], float], alpha: float, beta: float, y0: float, n: int) -> list:
+def rk4(
+    function: Callable[[float], float], alpha: float, beta: float, y0: float, n: int
+) -> list:
     """Solve ODE initial value problem using Runge-Kutta 4.
 
     Parameters
@@ -107,7 +109,7 @@ def rk4(function: Callable[[float], float], alpha: float, beta: float, y0: float
     values.append((x, y))
 
     for _ in range(n):
-        k1 = function(x,y)
+        k1 = function(x, y)
         k2 = function(x + h / 2, y + h * (k1 / 2))
         k3 = function(x + h / 2, y + h * (k2 / 2))
         k4 = function(x + h, y + h * k3)
@@ -115,6 +117,6 @@ def rk4(function: Callable[[float], float], alpha: float, beta: float, y0: float
         y += (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
         x += h
 
-        values.append((x,y))
-    
+        values.append((x, y))
+
     return values
